4c905777f5adff99f5ed42e17984bd4bc01adbff
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-24 12:57:31 -0800

    looped through data from array and displayed it on the page in unfactored format, going to refactor into functions next

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import { getTeams } from './fetch-utils.js';
 
 /* Get DOM Elements */
-const listEl = document.querySelector('main');
+const listEl = document.getElementById('list-container');
 
 /* State */
 let teamArray = [];
@@ -12,6 +12,23 @@ window.addEventListener('load', async () => {
     const teams = await getTeams();
     teamArray = teams;
     console.log(teamArray);
+    listEl.textContent = '';
+    for (let team of teamArray) {
+        const divEl = document.createElement('div');
+        const aEl = document.createElement('a');
+        const nameEl = document.createElement('p');
+        const imgEl = document.createElement('img');
+
+        nameEl.textContent = team.name;
+        imgEl.src = '';
+        aEl.href = '';
+
+        divEl.append(nameEl, imgEl);
+        aEl.append(divEl);
+        listEl.append(aEl);
+        // return listEl;
+    }
+    return listEl;
 });
 
 /* Display Functions */
